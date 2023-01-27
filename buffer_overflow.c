4f5b1f940ab3e888df3c3a7a6c03ed781cf12351
mohammmed kraydiye <45198347+mohammedkra@users.noreply.github.com>
2023-01-27 15:54:54 +0100

    Update buffer_overflow.c

--- buffer_overflow.c
+++ buffer_overflow.c
@@ -4,7 +4,7 @@
 const int winning_number = 4;
 const char correct_password[] = "lott";
 
-/* Lets the user play given number of rounds by the function start game */
+/* This function lets user play given number of rounds by the function start game */
 /* The range of the number is from 1 to 10 */
 /* Assume number of rounds is overwritten to become 11 for example */
 /* Then you will just simply enter 1 2 3 4 ... till you get the correct number */
@@ -24,7 +24,7 @@ int play_lottery(int turns) {
   return 0;
 }
 
-/* Lets user enter a password hardcoded consisting of 5 letters maximum */
+/* This function Lets user enter a password hardcoded consisting of 5 letters maximum */
 /* If password is correct then take us to the lottery */
 /* To crack this you will have to enter a very big input for the password */
 /* The very big input will overwrite correct_pass to make it non_zero */
